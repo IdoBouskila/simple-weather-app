8818e16887c6a168d3189cde612f4ee9ef7532ce
idobouskila <94786579+IdoBouskila@users.noreply.github.com>
2024-11-23 21:12:02 +0200

    feat: add dashboard with current weather status

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,58 @@
+import img from '../assets/3d-weather-icon.png'; // TODO: [-] Remove this line
+import { PiDropLight, PiEyeLight, PiSunLight, PiWindLight } from 'react-icons/pi';
+
+const Dashboard = () => {
+    return (
+        <div className='dashboard'>
+            <input
+                type='text'
+                className='searchbar'
+                placeholder='Search for location'
+            />
+
+            <div className='current-weather-container'>
+                <div className='title-container'>
+                    <h1>Current Weather</h1>
+                    <span className='time'>18:15</span>
+                </div>
+
+                <div className='weather-degrees-container'>
+                    <img
+                        src={ img }
+                        alt='weather-icon'
+                    />
+
+                    <div>
+                        <span className='degrees'>25</span>
+                        <span className='weather-description'>Clear Sky</span>
+                    </div>
+                </div>
+
+                <div className='weather-details'>
+                    <div>
+                        <PiWindLight />
+                        <span>5km/h</span>
+                    </div>
+
+                    <div>
+                        <PiDropLight />
+                        <span>80%</span>
+                    </div>
+
+                    <div>
+                        <PiSunLight />
+                        <span>3</span>
+                    </div>
+
+                    <div>
+                        <PiEyeLight />
+                        <span>10km</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Dashboard;
