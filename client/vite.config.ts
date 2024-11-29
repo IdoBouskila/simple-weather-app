4d39391b9aa81104cc1adc3ecdb4059c67cc5b53
idobouskila <94786579+IdoBouskila@users.noreply.github.com>
2024-11-29 13:43:44 +0200

    feat(config): add path aliases for improved module resolution

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@server': path.resolve(__dirname, '../server'),
+    },
+  },
   build: {
     rollupOptions: {
       external: ['leaflet'],
     },
   },
-})
+});
