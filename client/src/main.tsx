8444a7c0b93f6f0b252bd6bbb675ac2eab73a455
idobouskila <94786579+IdoBouskila@users.noreply.github.com>
2024-11-29 14:25:22 +0200

    feature(client): set-up `tRPC` in front-end

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,26 @@
 import App from './app.tsx';
 import { StrictMode } from 'react';
+import { trpc } from './utils/trpc.ts';
 import { createRoot } from 'react-dom/client';
+import { httpBatchLink } from '@trpc/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+const trpcClient = trpc.createClient({
+	links: [
+		httpBatchLink({
+			url: import.meta.env.VITE_API_URL,
+		}),
+	],
+});
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<trpc.Provider client={ trpcClient } queryClient={ queryClient }>
+			<QueryClientProvider client={ queryClient }>
+				<App />
+			</QueryClientProvider>
+		</trpc.Provider>
 	</StrictMode>
 );
