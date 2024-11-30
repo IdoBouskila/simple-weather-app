a4e2d202e884b5e294cf94c32216b9b73679d671
idobouskila <94786579+IdoBouskila@users.noreply.github.com>
2024-11-30 20:28:43 +0200

    feat(favorites): implement favorites functionality with persistent to local storage

---
+++ client/src/context/favorites-provider.tsx
@@ -0,0 +1,32 @@
+import { useContext, createContext } from 'react';
+import usePersistedFavorites from '@hooks/use-persisted-favorites';
+
+type FavoritesContextType = {
+    favorites: string[];
+    addFavorite: (name: string) => void;
+    removeFavorite: (name: string) => void;
+};
+
+const favoritesContext = createContext<FavoritesContextType | undefined>(undefined);
+
+export const useFavorites = () => {
+    const context = useContext(favoritesContext);
+
+    if(! context) {
+        throw new Error('useFavorites must be used within a FavoritesProvider');
+    }
+
+    return context;
+}
+
+export const FavoritesProvider: React.FC<{
+    children: React.ReactNode;
+}> = ({ children }) => {
+    const { favorites, addFavorite, removeFavorite } = usePersistedFavorites();
+
+    return (
+        <favoritesContext.Provider value={{ favorites, addFavorite, removeFavorite }}>
+            { children }
+        </favoritesContext.Provider>
+    );
+};
